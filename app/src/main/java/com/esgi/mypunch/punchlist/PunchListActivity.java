7657fca16148b6559f12124744d9556ecc79a0b1
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-05-21 14:53:29 +0200

    prepared activity

--- app/src/main/java/com/esgi/mypunch/punchlist/PunchListActivity.java
+++ app/src/main/java/com/esgi/mypunch/punchlist/PunchListActivity.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.esgi.mypunch.R;
+import com.esgi.mypunch.data.mainapi.PunchMyNodeProvider;
+
+import butterknife.ButterKnife;
 
 public class PunchListActivity extends AppCompatActivity {
+
+    public static final String TAG = "PunchListActivity";
+    private PunchMyNodeProvider provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_punch_list);
+
+        setTitle("Boxing sessions");
+        provider = new PunchMyNodeProvider();
+        ButterKnife.bind(this);
     }
 }
