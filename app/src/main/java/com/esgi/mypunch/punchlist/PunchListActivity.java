90d7363c4bc7f53dac61082be5c147a71d95627e
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-05-21 15:29:27 +0200

    session row design

--- app/src/main/java/com/esgi/mypunch/punchlist/PunchListActivity.java
+++ app/src/main/java/com/esgi/mypunch/punchlist/PunchListActivity.java
@@ -2,16 +2,20 @@ package com.esgi.mypunch.punchlist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 import com.esgi.mypunch.R;
 import com.esgi.mypunch.data.mainapi.PunchMyNodeProvider;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PunchListActivity extends AppCompatActivity {
 
     public static final String TAG = "PunchListActivity";
     private PunchMyNodeProvider provider;
+
+    @BindView(R.id.boxingSessionList) RecyclerView sessionsRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
