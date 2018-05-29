8fa2ac4df004f8e0ff81c1761971683366339410
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-05-29 22:12:20 +0200

    PunchListActivity to PunchListFragment

--- app/src/main/java/com/esgi/mypunch/login/LoginActivity.java
+++ app/src/main/java/com/esgi/mypunch/login/LoginActivity.java
@@ -10,7 +10,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.esgi.mypunch.R;
-import com.esgi.mypunch.punchlist.PunchListActivity;
+import com.esgi.mypunch.punchlist.PunchListFragment;
 import com.esgi.mypunch.register.RegisterActivity;
 
 import butterknife.BindView;
@@ -92,7 +92,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
 
     @Override
     public void navigateToPunchList() {
-        Intent intent = new Intent(this, PunchListActivity.class);
+        Intent intent = new Intent(this, PunchListFragment.class);
         startActivity(intent);
         finish();
     }
