af701b6fd3fb22f54e806600a89b51cce5a32e06
Benjamin Metaut <metautbenjamin@hotmail.fr>
2018-07-13 16:58:43 +0200

    quick fix on chart

--- app/src/main/java/com/esgi/mypunch/Synthesis/SynthesisFragment.java
+++ app/src/main/java/com/esgi/mypunch/Synthesis/SynthesisFragment.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.esgi.mypunch.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -24,29 +28,43 @@ public class SynthesisFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_synthesis, container, false);
-        LineChart chart = (LineChart) view.findViewById(R.id.chart);
+        BarChart chart = (BarChart) view.findViewById(R.id.chart);
 
 
-        int[] dataObjects = {56};
+        int[] dataObjects = {56, 255, 0, 23, 135, 68};
+        int i = 0;
 
-        List<Entry> entries = new ArrayList<Entry>();
+        List<BarEntry> entries = new ArrayList<BarEntry>();
 
         for (int data : dataObjects) {
 
             // turn your data into Entry objects
-            entries.add(new Entry(data, data));
+            entries.add(new BarEntry(i, data));
+            i++;
         }
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        BarDataSet dataSet = new BarDataSet(entries, "Label"); // add entries to dataset
 
-        LineData lineData = new LineData(dataSet);
+        BarData lineData = new BarData(dataSet);
 
         chart.setData(lineData);
         chart.getAxisLeft().setDrawGridLines(false);
         chart.getAxisRight().setDrawGridLines(false);
 
-      //  chart.getXAxis().setDrawGridLines(false);
-        chart.invalidate();
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setEnabled(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getAxisLeft().setDrawAxisLine(false);
+        chart.getAxisRight().setDrawAxisLine(false);
 
+        chart.setEnabled(false);
+        chart.invalidate();
+        chart.setTouchEnabled(false);
+        chart.setDragEnabled(false);
+        chart.setScaleEnabled(false);
+        chart.setScaleXEnabled(false);
+        chart.setScaleYEnabled(false);
+        chart.setPinchZoom(false);
+        chart.setDoubleTapToZoomEnabled(false);
 
         return view;
     }
